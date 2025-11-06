699f809979de31078d12da497a1ac9e2f71abb3d
MaikoVosPXL <12401562@student.pxl.be>
2025-11-06 09:23:01 +0100

    Added NavigationComponent folders & invoke

---
+++ MvcNewSportStore/MvcSportStore/MvcSportStore/Components/NavigationMenuViewComponent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcSportStore.Data;
+
+namespace MvcSportStore.Components
+{
+    public class NavigationMenuViewComponent : ViewComponent
+    {
+        private StoreDbContext _context;
+        public NavigationMenuViewComponent(StoreDbContext context)
+        {
+            _context = context;
+        }
+        public IViewComponentResult Invoke()
+        {
+            return View(
+                _context.Products
+                .Select(x => x.Category)
+                .Distinct()
+                .OrderBy(x => x));
+        }
+    }
+}
